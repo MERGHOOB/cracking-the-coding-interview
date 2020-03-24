60cded96dbca1c7da379136592b0218c92288919
mekhan <margub.khan@gmail.com>
2020-03-24 14:13:04 +0530

    chaptor-10 ch10_1_SortedMerge

---
+++ chap10/src/com/mentor/questions/ch10_1_SortedMerge.java
@@ -0,0 +1,29 @@
+package com.mentor.questions;
+
+
+/*
+two sorted arrays A and B
+Merge both of them inplace
+
+A have enough buffer to hold.
+ */
+public class ch10_1_SortedMerge {
+
+    void sort(int [] a, int [] b) {
+
+        int end = a.length-1;
+
+        int bLast = b.length-1;
+        int aLast = a.length-b.length-1;
+
+        while(aLast >=0 && bLast >= 0) {
+            if(a[aLast] > b[bLast]) {
+                a[end--] = a[aLast--];
+            }
+            else {
+                a[end--] = b[bLast--];
+            }
+        }
+    }
+
+}
